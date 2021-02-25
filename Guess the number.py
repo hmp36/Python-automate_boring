26306dab10186a4eabf357ddf477fef3849eafb4
Hagan <haganpratt@gmail.com>
2021-02-24 23:52:58 -0500

    guessing game

--- Guess the number.py
+++ Guess the number.py
@@ -5,7 +5,7 @@ print ('Hello. What is your name?')
 
 name = input()
 secretNumber = random.randint(1, 20)
-print (''+ name +'?!' ' Hell yeah!' ' That is a really cool name! ' + name+ ', I am thinking of a number between 1 and 20. What is it? ')
+print (''+ name +'?!' ' Cool!' ' That is a very fine name! ' + name+ ', I am thinking of a number between 1 and 20. What is it? ')
 
 
 # ask the player to guess 6 times
@@ -23,6 +23,6 @@ for guessesTaken in range (1,7):
 #This condition is the correct guess!
 
 if guess == secretNumber:
-    print('Nice, ' + name + '.' ' You nailed it!' 'You guessed in ' + str(guessesTaken) + ' guesses')
+    print('Nice, ' + name + '.' ' You nailed it!' ' You guessed in ' + str(guessesTaken) + ' guesses.')
 else:
-    print ('Nope. Sorry, man. The number was '+ str(secretNumber) + '. Better luck next time.')
+    print ('Nope. Sorry, man. The number was '+ str(secretNumber) + '. Better luck next time!')
