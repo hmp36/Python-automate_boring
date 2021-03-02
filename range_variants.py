610be524f5f383e09ce801e150f7ce6a6b8e94b0
Hagan <haganpratt@gmail.com>
2021-03-01 22:37:27 -0500

    python practice

---
+++ range_variants.py
@@ -0,0 +1,3 @@
+print('My name is')
+for i in range(5, -1, -1):
+    print('Jimmy Five Times ' + str(i))
---
+++ your_name.py
@@ -0,0 +1,14 @@
+name = ''
+while name != 'your name':
+    print('Please type your name.')
+    name = input()
+    if name == 'your name':
+        break
+print ('Thank you!') 
+
+
+
+#This condition creates an infinite loop.
+#while True:
+    #print('Hello!')
+    
