f8c53ca6922d1fe969267b6357a6e647e98e23b9
Hagan <haganpratt@gmail.com>
2021-03-03 00:06:24 -0500

    auto_boring exercises

---
+++ lists.py
@@ -0,0 +1,17 @@
+supplies = ['pens', 'staplers', 'flame-throwers', 'binders']
+for i in range(len(supplies)):
+    print('index' + str(i) + 'in supplies is: ' + supplies[i])
+
+
+
+
+#copied from shell
+#>>> a = 'AAA'
+#>>> b = 'BBB'
+#>>> a, b = b, a
+#>>> a
+#'BBB'
+#>>> b
+#'AAA'
+#>>> 
+    
